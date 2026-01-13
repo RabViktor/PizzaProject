24b6fb188ee96e204a712582bbe025c905b292e9
Bonzsii <szucs.zsombor@kreativszolnok.hu>
2026-01-13 08:58:17 +0100

    Loadingspinner.jsx, Loadingspinner.css  megírva

---
+++ Frontend/VzsPizzaBar/src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import "./LoadingSpinner.css"
+
+export function LoadingSpinner(){
+    return (
+        <div className="spinner-div">
+            <div className="loading-spinner"></div>
+        </div>
+    )
+}
