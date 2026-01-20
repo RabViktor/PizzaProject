38c40544900cbc135ddc9c85a0e6c04d3e71ec2e
RabViktor <rab.viktor@kreativszolnok.hu>
2026-01-20 09:46:52 +0100

    ProfilInfor.jsx, profileRoutes.js szerkesztve

--- Frontend/VzsPizzaBar/src/components/ProfileInfo.jsx
+++ Frontend/VzsPizzaBar/src/components/ProfileInfo.jsx
@@ -1,9 +1,87 @@
 import { useState } from 'react'
 import './ProfileInfo.css'
+import { useEffect } from 'react'
+import { useCart } from '../context/CartContext'
 
 export function ProfileInfo(){
 
     const [activeBtn, setActiveBtn] = useState(false)
+    const [error, setError] = useState(null)
+
+    const { showToast } = useCart()
+
+    const [profile, setProfile] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        postcode: "",
+        city: "",
+        roadnum: ""
+    })
+
+    const fields = [
+        {key: "name", label: "Név", editable: true },
+        {key: "email", label: "E-mail", editable: true},
+        {key: "phone", label: "Tel.szám" , editable: true},
+        {key: "postcode", label: "Irányító sz." , editable: true},
+        {key: "city", label: "Település" , editable: true},
+        {key: "roadnum", label: "Utca / házszám" , editable: true}
+    ]
+
+
+    useEffect(() => {
+        const loadProfile = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                const res = await fetch("http://localhost:5000/api/profile/me", {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+
+            if (!res.ok) {
+                setError("Nem sikerült betölteni a profilt")
+                return
+            }
+
+                const data = await res.json()
+                setProfile(data)
+            } catch (err) {
+                setError(err.message)
+            }
+        };
+
+        loadProfile()
+    }, [])
+
+
+
+    const saveProfile = async () => {
+        try {
+            const token = localStorage.getItem("token")
+
+            const res = await fetch("http://localhost:5000/api/profile/update", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+            },
+                body: JSON.stringify(profile)
+            })
+
+            if (!res.ok) {
+                setError("Nem sikerült a profil frissítése")
+            return;
+            }
+
+            const data = await res.json()
+            setActiveBtn(false)
+            showToast('Sikeres mentés')
+
+        } catch (err) {
+            setError(err.message)
+        }
+
+    }
+
 
     return(
         <>
@@ -18,21 +96,25 @@ export function ProfileInfo(){
                 </div>
                 <div id="data-div">
                     <div id="profile-datas">
-                        <div className='data'>
-                            <h3>Név: </h3>
-                            {activeBtn && <input type="text" />}
-                            {!activeBtn && <h3>Szűcs Zsombor</h3>}
-                            
-                        </div>
-                        <div className='data'>
-                            <h3>E-mail: </h3>
-                            {activeBtn && <input type="text" />}
-                            {!activeBtn && <h3>example@email.com</h3>}
-                        </div>
+                            {fields.map(a => (
+                                <div className="data" key={a.key}>
+                                    <h3>{a.label}:</h3>
+                                    {activeBtn && a.editable ? (
+                                        <input type="text"
+                                            value={profile[a.key] || ""}
+                                            onChange={e => {
+                                                setProfile({...profile, [a.key]: e.target.value})
+                                            }}    
+                                        />
+                                    ): (
+                                        <h3>{profile[a.key]}</h3>
+                                    )}
+                                </div>
+                            ))}
                     </div>
                 </div>
                 <div id="save-div">
-                    <button disabled={!activeBtn}>Mentés</button>
+                    <button onClick={saveProfile} disabled={!activeBtn}>Mentés</button>
                 </div>
                 
             </div>
