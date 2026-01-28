60cee09b39412065fb8a3b6bf1645ac19b9825d0
Bonzsii <szucs.zsombor@kreativszolnok.hu>
2026-01-28 08:11:11 +0100

    Contact.jsx,Contact.css változtatva

--- Frontend/VzsPizzaBar/src/pages/Contact.jsx
+++ Frontend/VzsPizzaBar/src/pages/Contact.jsx
@@ -7,17 +7,17 @@ export function Contact(){
            <section className="contact-info">
             <div className="contact-cards">
                 <div className="contact-card">
-                    <img src="" alt="" />
+                    <img className="kep" src="/location.png" alt="" />
                     <p className="con">Helyszín</p>
                     <p className="desc"> 5000 Szolnok, Áchim András utca 12-14</p>
                 </div>
                 <div className="contact-card">
-                    <img src="" alt="" />
+                    <img className="kepp" src="/mail.png" alt="" />
                     <p className="con">E-mail</p>
                     <p className="desc">noreply@vzspizzabar.hu</p>
                 </div>
                 <div className="contact-card">
-                    <img src="" alt="" />
+                    <img className="kep" src="/phone.png" alt="" />
                     <p className="con">Telefon</p>
                     <p className="desc">+36 70 123 4567</p>
                 </div>
