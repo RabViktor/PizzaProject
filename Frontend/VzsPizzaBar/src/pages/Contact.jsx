64b64adbeb1b657dd2c53064aa742c9fbbb0a75e
Bonzsii <szucs.zsombor@kreativszolnok.hu>
2026-01-29 08:52:55 +0100

    Contact.jsx,Contact.css változtatva

--- Frontend/VzsPizzaBar/src/pages/Contact.jsx
+++ Frontend/VzsPizzaBar/src/pages/Contact.jsx
@@ -17,12 +17,48 @@ export function Contact(){
                     <p className="desc">noreply@vzspizzabar.hu</p>
                 </div>
                 <div className="contact-card">
-                    <img className="kep" src="/phone.png" alt="" />
+                    <img className="kepo" src="/phone.png" alt="" />
                     <p className="con">Telefon</p>
                     <p className="desc">+36 70 123 4567</p>
                 </div>
             </div>
            </section>
+
+           <div className="contact-div">
+            <form className="contact-form">
+                <div className="login-h2">
+                    <h2>Írj nekünk</h2>
+                </div>
+
+                <div style={{ display: "flex", alignItems: "center", flexDirection: "column", padding: "0 50px" }}>
+                <div>
+                  <label htmlFor="name">Teljes név:</label><br />
+                  <input
+                    id="name"
+                    type="text"
+                    name="name"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email">E-mail cím:</label><br />
+                  <input
+                    id="email"
+                    type="text"
+                    name="email"
+                  />
+                </div>
+                <div>
+                    <label htmlFor="message"></label><br />
+                    <textarea className="mess"></textarea>
+                </div>
+                </div>
+
+                <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+                <button type="submit" className="con-btn">Küldés</button>
+                </div>
+
+            </form>
+           </div>
            
         </>
     )
