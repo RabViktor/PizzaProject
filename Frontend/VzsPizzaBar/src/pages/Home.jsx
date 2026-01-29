d1572c0fe577153bd62687dc4fec774c16e88afb
Bonzsii <szucs.zsombor@kreativszolnok.hu>
2026-01-29 12:36:31 +0100

    Home.jsx

--- Frontend/VzsPizzaBar/src/pages/Home.jsx
+++ Frontend/VzsPizzaBar/src/pages/Home.jsx
@@ -20,27 +20,27 @@ export function Home(){
                 <h2>Kategóriák</h2>
                 <div className="category-grid">
                     <div className="category-card" onClick={() => navi('/menu?category=pizza')}>
-                        <img src="/categories/pizza.jpg" alt="Pizza" />
+                        <img src="/categories/kolbaszos.jpg" alt="Pizza" />
                         <button>Pizza</button>
                     </div>
                     <div className="category-card" onClick={() => navi('/menu?category=gyros')}>
-                        <img src="/categories/gyros.jpg" alt="Gyros" />
+                        <img src="/categories/Falafel_gyro.png" alt="Gyros" />
                         <button>Gyros</button>
                     </div>
                     <div className="category-card" onClick={() => navi('/menu?category=chicken')}>
-                        <img src="/categories/chicken.png" alt="Csirke" />
+                        <img src="/categories/12wing.png" alt="Csirke" />
                         <button>Csirke</button>
                     </div>
                     <div className="category-card" onClick={() => navi('/menu?category=sauce')}>
-                        <img src="/categories/sauce.jpg" alt="Szósz" />
+                        <img src="/categories/salsa.jpg" alt="Szósz" />
                         <button>Szósz</button>
                     </div>
                     <div className="category-card" onClick={() => navi('/menu?category=dessert')}>
-                        <img src="/categories/dessert.jpg" alt="Desszert" />
+                        <img src="/categories/magnum_lemonade.jpg" alt="Desszert" />
                         <button>Desszert</button>
                     </div>
                     <div className="category-card" onClick={() => navi('/menu?category=drink')}>
-                        <img src="/categories/drink.jpg" alt="Ital" />
+                        <img src="/categories/033PepsiBlack (1).jpg" alt="Ital" />
                         <button>Ital</button>
                     </div>
                 </div>
