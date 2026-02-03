0917d06d8be5db68c4dd85713f5457a708e4b22b
RabViktor <rab.viktor@kreativszolnok.hu>
2026-02-03 09:41:37 +0100

    checout.js létrehozva

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,7 +4,7 @@ import cors from "cors"
 import productsRoute from "./routes/products.js"
 import authRoute from "./routes/auth.js"
 import profileRoutes from "./routes/profileRoutes.js"
-import Stripe from "stripe"
+import checkoutRoutes from "./src/routes/checkout.js";
 
 dotenv.config()
 
@@ -12,47 +12,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-app.post("/api/create-checkout-session", async (req, res) => {
-    try {
-        const { items, customerData } = req.body;
-
-        const line_items = items.map(item => ({
-            price_data: {
-                currency: "huf",
-                product_data: { name: item.name },
-                unit_amount: item.price * 100
-            },
-            quantity: item.quantity
-        }));
-
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            mode: "payment",
-            line_items,
-            customer_email: customerData.email,
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cart",
-            metadata: {
-                name: customerData.name,
-                phone: customerData.phone,
-                city: customerData.city,
-                roadNum: customerData.roadNum,
-                comment: customerData.comment,
-                payment: customerData.payment
-            }
-        });
-
-        res.json({ url: session.url });
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "stripe hiba" });
-    }
-});
-
-
+app.use("/api", checkoutRoutes);
 app.use("/api/products", productsRoute)
 app.use("/api/auth", authRoute)
 app.use("/api/profile", profileRoutes)
